475d9aa6d66c2b6a26ddfe28843ed42a8fff857c
Alex <omtmn@icloud.com>
2021-09-14 02:36:08 -0400

    minor changes in things I do not need

--- amazon/src/context/reducer.js
+++ amazon/src/context/reducer.js
@@ -1,13 +1,11 @@
 export const initialState = {
     cart: [],
-    user: null,
 };
 
 export const getCartTotal = (cart) => 
 cart?.reduce((amount, item) => item.price + amount, 0);
 
 const reducer = (state, action) => {
-    console.log(action)
     switch(action.type) {
         case 'ADD_TO_CART':
             return { 
--- amazon/src/index.js
+++ amazon/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './components/App';
-import reportWebVitals from './context/reportWebVitals';
 import { StateProvider } from "./context/StateProvider";
 import reducer, { initialState } from './context/reducer';
 
@@ -14,8 +13,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
